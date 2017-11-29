e1fa7a99e4e03a71732be076ce144908c9392a7c
Giuliano Garces <garces1989@gmail.com>
2017-11-29 00:52:25 -0300

    Update navbar2 sección Inicio a redirección a index wordpress

--- saesa-ev/php/navbar2.php
+++ saesa-ev/php/navbar2.php
@@ -8,7 +8,7 @@
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="./"><b>Inicio</b></a>
+    <a class="navbar-brand" href="../"><b>Inicio</b></a>
   </div>
 
   <!-- Collect the nav links, forms, and other content for toggling -->
